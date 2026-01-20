dccc86d23f3898bb42381234fa7344000df07f92
Nan Xiao <me@nanx.me>
2026-01-20 13:56:37 -0500

    Add `@importFrom` directives to fix check note

--- R/gsDesignTune-package.R
+++ R/gsDesignTune-package.R
@@ -2,5 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom future.apply future_lapply
+#' @importFrom progressr progressor with_progress
+#' @importFrom R6 R6Class
 ## usethis namespace: end
 NULL
